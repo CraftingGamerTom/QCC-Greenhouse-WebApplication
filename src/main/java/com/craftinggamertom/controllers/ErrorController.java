845661560a2441088a123d9fe57d18baed511bbb
Thomas Rokicki <TomWRHS@gmail.com>
2017-11-15 20:15:20 -0500

    Updated 404 Error page
    Clean, crisp feel to the 404 page. Because we see it so often in testing. Enjoy!

--- src/main/java/com/craftinggamertom/controllers/ErrorController.java
+++ src/main/java/com/craftinggamertom/controllers/ErrorController.java
@@ -36,7 +36,7 @@ public class ErrorController {
                 break;
             }
             case 404: {
-                errorMsg = "Http Error Code: 404. Resource not found";
+                errorMsg = get404ErrorMessage(); // Gets the html for the page
                 break;
             }
             case 500: {
@@ -58,5 +58,27 @@ public class ErrorController {
     private int getErrorCode(HttpServletRequest httpRequest) {
         return (Integer) httpRequest
           .getAttribute("javax.servlet.error.status_code");
+    }
+    
+    /**
+     * To keep the class organized the strings can be accessed through methods. This is for the 404 page
+     * @return
+     */
+    private String get404ErrorMessage() {
+    	String message = "";
+    	message +="\r\n" + 
+    			"    <div class=\"middle-box text-center animated fadeInDown\">\r\n" + 
+    			"        <h1>404</h1>\r\n" + 
+    			"        <h3 class=\"font-bold\">Page Not Found</h3>\r\n" + 
+    			"        <h5>Http Error Code: 404. Resource not found</h5>" +
+    			"\r\n" + 
+    			"        <div class=\"error-desc\">\r\n" + 
+    			"            Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.\r\n" + 
+    			"            <br/><a href=\"/\" class=\"btn btn-primary m-t\">Home</a>" + 
+    			"        </div>\r\n" + 
+    			"    </div>\r\n" + 
+    			"";
+    	
+    	return message;
     }
 }
