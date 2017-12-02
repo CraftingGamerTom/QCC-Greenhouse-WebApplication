74620e042369cb0f0cee0a5bbefdf8f25f127f5f
Thomas Rokicki <TomWRHS@gmail.com>
2017-12-02 00:03:30 -0500

    Update 404 message
    Made the 404 page more cute

--- src/main/java/com/craftinggamertom/controllers/ErrorController.java
+++ src/main/java/com/craftinggamertom/controllers/ErrorController.java
@@ -12,64 +12,64 @@ import org.springframework.web.servlet.ModelAndView;
 public class ErrorController {
 	@RequestMapping(value = "/errors", method = RequestMethod.GET)
 	public ModelAndView goToErrorPage(HttpServletRequest httpRequest, Model model) {
-        
-        //Everything below for printing the error on web page
-        String errorMsg = "";
-        int httpErrorCode = getErrorCode(httpRequest);
-        
-        switch (httpErrorCode) {
-            case 400: {
-                errorMsg = "Http Error Code: 400. Bad Request";
-                break;
-            }
-            case 401: {
-                errorMsg = "Http Error Code: 401. Unauthorized";
-                break;
-            }
-            case 404: {
-                errorMsg = get404ErrorMessage(); // Gets the html for the page
-                break;
-            }
-            case 500: {
-                errorMsg = "Http Error Code: 500. Internal Server Error";
-                break;
-            }
-        }
-        model.addAttribute("error-message", errorMsg);
-        
-        
-        return new ModelAndView("errorPage");
-    }
-     
+
+		// Everything below for printing the error on web page
+		String errorMsg = "";
+		int httpErrorCode = getErrorCode(httpRequest);
+
+		switch (httpErrorCode) {
+		case 400: {
+			errorMsg = "Http Error Code: 400. Bad Request";
+			break;
+		}
+		case 401: {
+			errorMsg = "Http Error Code: 401. Unauthorized";
+			break;
+		}
+		case 404: {
+			errorMsg = get404ErrorMessage(); // Gets the html for the page
+			break;
+		}
+		case 500: {
+			errorMsg = "Http Error Code: 500. Internal Server Error";
+			break;
+		}
+		}
+		model.addAttribute("error-message", errorMsg);
+
+		return new ModelAndView("errorPage");
+	}
+
 	/**
 	 * gets the error code for the web page
+	 * 
 	 * @param httpRequest
 	 * @return
 	 */
-    private int getErrorCode(HttpServletRequest httpRequest) {
-        return (Integer) httpRequest
-          .getAttribute("javax.servlet.error.status_code");
-    }
-    
-    /**
-     * To keep the class organized the strings can be accessed through methods. This is for the 404 page
-     * @return
-     */
-    private String get404ErrorMessage() {
-    	String message = "";
-    	message +="\r\n" + 
-    			"    <div class=\"middle-box text-center animated fadeInDown\">\r\n" + 
-    			"        <h1>404</h1>\r\n" + 
-    			"        <h3 class=\"font-bold\">Page Not Found</h3>\r\n" + 
-    			"        <h5>Http Error Code: 404. Resource not found</h5>" +
-    			"\r\n" + 
-    			"        <div class=\"error-desc\">\r\n" + 
-    			"            Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.\r\n" + 
-    			"            <br/><a href=\"/\" class=\"btn btn-primary m-t\">Home</a>" + 
-    			"        </div>\r\n" + 
-    			"    </div>\r\n" + 
-    			"";
-    	
-    	return message;
-    }
+	private int getErrorCode(HttpServletRequest httpRequest) {
+		return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+	}
+
+	/**
+	 * To keep the class organized the strings can be accessed through methods. This
+	 * is for the 404 page
+	 * 
+	 * @return
+	 */
+	private String get404ErrorMessage() {
+		String message = "";
+		message += "\r\n" + "    <div class=\"middle-box text-center animated fadeInDown\">\r\n"
+				+ "<img alt=\"404_potato\" class=\"img-responsive\"\r\n"
+				+ "								src=\"http://drive.google.com/uc?export=view&id=1O8W05_OJNMZHsLu4etvfMNXwtjmRN81F\">\r\n"
+				+ "            Why do potatoes make good detectives?\r\n"
+				+ "            Because they keep their eyes peeled.\r\n"
+				+ "            We can't find the page you requested but maybe he can.\r\n" + "\r\n"
+				+ "        <div class=\"error-desc\">\r\n" + "        <h2>404</h2>\r\n"
+				+ "        <h4 class=\"font-bold\">Page Not Found</h4>\r\n"
+				+ "        <h5>Http Error Code: 404. Resource not found</h5>"
+				+ "            <br/><a href=\"/\" class=\"btn btn-primary m-t\">Home</a>" + "        </div>\r\n"
+				+ "    </div>\r\n" + "";
+
+		return message;
+	}
 }
