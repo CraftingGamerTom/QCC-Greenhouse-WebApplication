ed4fbb6a0bdfbd0d833dab8cdfc2e500db11bda1
Thomas Rokicki <TomWRHS@gmail.com>
2017-12-04 12:08:39 -0500

    Updated functions
    New functions for more implementations of authorization.
    Method names were also changed accordingly.

--- src/main/java/com/craftinggamertom/security/authorization/PageAuthority.java
+++ src/main/java/com/craftinggamertom/security/authorization/PageAuthority.java
@@ -17,9 +17,43 @@ public class PageAuthority extends Authority{
 	 * @return true if the authority passed in is equal to or greater than the
 	 *         authority of the page
 	 */
-	public boolean grantAccessTo(UserAuthority userAuthority) {
+	public boolean grantAccessGTE(UserAuthority userAuthority) {
 		boolean grantAccess = false;
 		if (userAuthority.getAuthorityLevel() >= getAuthorityLevel()) {
+			grantAccess = true;
+		}
+
+		return grantAccess;
+	}
+	
+	/**
+	 * Checks if the Authority level is less than or equal to the authority level of the page.
+	 * 
+	 * 
+	 * @param userAuthority
+	 * @return true if the authority passed in is equal to or less than the
+	 *         authority of the page
+	 */
+	public boolean grantAccessLTE(UserAuthority userAuthority) {
+		boolean grantAccess = false;
+		if (userAuthority.getAuthorityLevel() <= getAuthorityLevel()) {
+			grantAccess = true;
+		}
+
+		return grantAccess;
+	}
+	
+	/**
+	 * Checks if the Authority level is equal to the authority level of the page.
+	 * 
+	 * 
+	 * @param userAuthority
+	 * @return true if the authority passed in is equal to or less than the
+	 *         authority of the page
+	 */
+	public boolean grantAccessEqual(UserAuthority userAuthority) {
+		boolean grantAccess = false;
+		if (userAuthority.getAuthorityLevel() == getAuthorityLevel()) {
 			grantAccess = true;
 		}
 
