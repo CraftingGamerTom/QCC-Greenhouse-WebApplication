645f8f83b0cd90fc62f574c9dd1e9771c2839454
Thomas Rokicki <TomWRHS@gmail.com>
2017-12-11 16:10:22 -0500

    Fixed Error handling
    There is only one error page displayed now. No more confusion.

--- src/main/java/com/craftinggamertom/controllers/ErrorController.java
+++ src/main/java/com/craftinggamertom/controllers/ErrorController.java
@@ -8,14 +8,21 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.craftinggamertom.constants.JSPLocation;
 
+@ControllerAdvice
+@EnableWebMvc
 @Controller
 public class ErrorController {
+
+	@ExceptionHandler(value = Exception.class)
 	@RequestMapping(value = "/errors", method = RequestMethod.GET)
 	public ModelAndView goToErrorPage(HttpServletRequest httpRequest, Model model) {
 
