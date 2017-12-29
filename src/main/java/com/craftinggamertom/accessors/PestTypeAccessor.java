23beb643b8c2df1b23f9136745f211b4b1c3dc69
Thomas Rokicki <TomWRHS@gmail.com>
2017-12-28 22:01:00 -0500

    Added Accessors

---
+++ src/main/java/com/craftinggamertom/accessors/PestTypeAccessor.java
@@ -0,0 +1,9 @@
+/**
+* Copyright (c) 2017 Thomas Rokicki
+*/
+
+package com.craftinggamertom.accessors;
+
+public class PestTypeAccessor extends Accessor{
+
+}
---
+++ src/main/java/com/craftinggamertom/accessors/ZoneAccessor.java
@@ -0,0 +1,9 @@
+/**
+* Copyright (c) 2017 Thomas Rokicki
+*/
+
+package com.craftinggamertom.accessors;
+
+public class ZoneAccessor extends Accessor{
+
+}
