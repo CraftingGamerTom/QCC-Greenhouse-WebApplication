e1a71ec0c0ff63ab3b633b8ad40a56bf7fca64da
Thomas Rokicki <TomWRHS@gmail.com>
2017-12-29 22:09:44 -0500

    Set up Accessors

--- src/main/java/com/craftinggamertom/accessors/PlantTypeAccessor.java
+++ src/main/java/com/craftinggamertom/accessors/PlantTypeAccessor.java
@@ -1,29 +1,46 @@
 /**
-* Copyright (c) 2017 Thomas Rokicki
-*/
+ * Copyright (c) 2017 Thomas Rokicki
+ */
 
 package com.craftinggamertom.accessors;
 
 import com.craftinggamertom.database.ConfigurationReaderSingleton;
 import com.craftinggamertom.entity.PlantType;
 
-public class PlantTypeAccessor extends Accessor{
-	
+public class PlantTypeAccessor extends Accessor {
+
 	public PlantTypeAccessor() {
 		super();
-		this.collection = ConfigurationReaderSingleton.getPlantTypeCollection();
+		collection = database.getCollection(ConfigurationReaderSingleton.getPlantTypeCollection());
 	}
 
 	public boolean storeInDatabase(PlantType plantType) {
 		return super.storeInDatabase(plantType.getEntityAsDocument());
 	}
 
-	public boolean updateEntity() {
-		return false;
+	/**
+	 * Updates the entity by replacement
+	 * 
+	 * @param the
+	 *            plant type to be updated
+	 * @param the
+	 *            updated plant type
+	 * @return true if successful
+	 */
+	public boolean updateEntity(PlantType oldPlantType, PlantType newPlantType) {
+		return super.updateEntity(oldPlantType.getEntityAsDocument(), newPlantType.getEntityAsDocument());
 	}
 
-	public boolean deleteEntity() {
-		return false;
+	/**
+	 * Deletes the plant type
+	 * This is very dangerous currently
+	 * 
+	 * @param plantType
+	 *            to be deleted
+	 * @return true if successful
+	 */
+	public boolean deleteEntity(PlantType plantType) {
+		return super.deleteEntity(plantType.getEntityAsDocument());
 	}
 
 }
